f92bb9fc21edcee56cfc8e829ead97be5f01e465
Sadi Yumuşak <sadiyumusak@gmail.com>
2014-03-24 19:59:01 +0200

    no root pwd to change icon

--- indicator-chars.py
+++ indicator-chars.py
@@ -124,10 +124,10 @@ class IndicatorChars:
         cb.set_text(char)
 
     def DarkTheme(self, dude):
-	os.system("sudo /usr/local/indicator-chars/dark-theme-icon.sh && restart.sh")
+	os.system("sudo /usr/local/indicator-chars/dark-theme-icon && restart")
 
     def LightTheme(self, dude):
-	os.system("sudo /usr/local/indicator-chars/light-theme-icon.sh && restart.sh")
+	os.system("sudo /usr/local/indicator-chars/light-theme-icon && restart")
 
     def on_quit(self, widget):
         gtk.main_quit()
