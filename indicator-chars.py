33061b73c2afb49b82fad9ac1e9a2307c50951b5
Denilson Sá <denilsonsa@gmail.com>
2014-05-26 03:05:53 -0300

    Some code cleanup.
    I should have split this commit into smaller pieces. Anyway, this is the
    list of changes:
    
    * Refactoring the update_menu method:
        * Split that method into 3 methods: one for updating the GTK+ menu,
          one for parsing a single config line, one for reading/parsing the
          config file.
        * Parsing a config line includes doctests, which serves as both
          tests and documentation.
    * Adding --test parameter to run the doctests.
    * Forcing Python 2.x at the shebang.
    * Removing trailing whitespace.
    * Changing double-quotes to single-quotes.

--- indicator-chars.py
+++ indicator-chars.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 #
 # Very simple chars indicator.
@@ -6,16 +6,16 @@
 #
 # Copyright (c) 2011, Tobias Schlitt
 # All rights reserved.
-# 
+#
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
-# 
+#
 # Redistributions of source code must retain the above copyright notice,
 # this list of conditions and the following disclaimer.  Redistributions
 # in binary form must reproduce the above copyright notice, this list of
 # conditions and the following disclaimer in the documentation and/or
 # other materials provided with the distribution.
-# 
+#
 # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
 # "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
 # LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
@@ -37,6 +37,7 @@ import gio
 import signal
 import subprocess
 import appindicator
+import sys
 
 APP_NAME = 'indicator-chars'
 APP_VERSION = '0.2'
@@ -45,17 +46,14 @@ class IndicatorChars:
     CHARS_PATH = os.path.join(os.getenv('HOME'), '.indicator-chars')
     SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
 
-    submenu_title_pattern = re.compile(r'\[([^]]+)\] *')
-    description_pattern = re.compile(r' *(\([^)]+\)) *')
-
     def __init__(self):
         self.ind = appindicator.Indicator(
             # Custom icon seems to doesn't work on my Ubuntu 12.04 LTS running Unity 2D
-            #"Chars", os.path.join(self.SCRIPT_DIR, 'light16x16.png'),
+            #'Chars', os.path.join(self.SCRIPT_DIR, 'light16x16.png'),
             # So fallback to an referenced theme's icon name
-            "Chars", "accessories-character-map",
+            'Chars', 'accessories-character-map',
             appindicator.CATEGORY_APPLICATION_STATUS)
-        self.ind.set_status(appindicator.STATUS_ACTIVE)        
+        self.ind.set_status(appindicator.STATUS_ACTIVE)
 
         self.update_menu()
 
@@ -66,77 +64,138 @@ class IndicatorChars:
 
     def on_chars_changed(self, filemonitor, file, other_file, event_type):
         if event_type == gio.FILE_MONITOR_EVENT_CHANGES_DONE_HINT:
-            print 'Characters changed, updating menu...'
+            #print 'Characters changed, updating menu...'
             self.update_menu()
-    
-    def update_menu(self, widget = None, data = None):
+
+    @staticmethod
+    def parse_line(line):
+        '''Parses a line from the configuration file.
+
+        Returns a tuple (title, items), where items is a list of tuples
+        (character, label).
+
+        >>> IndicatorChars.parse_line('abc')
+        (u'abc', [(u'a', u'a'), (u'b', u'b'), (u'c', u'c')])
+        >>> IndicatorChars.parse_line('  a  b  c  ')
+        (u'abc', [(u'a', u'a'), (u'b', u'b'), (u'c', u'c')])
+        >>> IndicatorChars.parse_line(' [ title ]  a  b  c  ')
+        (u' title ', [(u'a', u'a'), (u'b', u'b'), (u'c', u'c')])
+        >>> IndicatorChars.parse_line(' [ title ]  a  b   (foobar)    c  ')
+        (u' title ', [(u'a', u'a'), (u'b', u'b (foobar)'), (u'c', u'c')])
+        >>> IndicatorChars.parse_line(' [ title ]  ab(foobar)c  ')
+        (u' title ', [(u'a', u'a'), (u'b', u'b (foobar)'), (u'c', u'c')])
+        >>>
+        '''
+        title = None
+        items = []
+
+        submenu_title_pattern = re.compile(r'\[([^]]+)\] *')
+        description_pattern = re.compile(r'(.) *(\([^)]+\))? *')
+
+        # Making sure the string is unicode and removing extra whitespace.
+        line = unicode(line, 'utf8', 'replace').strip()
+
+        # Checking for '[title]'.
+        submenu_match = submenu_title_pattern.match(line)
+        if submenu_match:
+            title = submenu_match.group(1)
+            # Removing title part from line.
+            line = line[submenu_match.end():].strip()
+
+        # Iterating over line to get all characters.
+        while line:
+            description_match = description_pattern.match(line)
+            if not description_match:
+                # Something very wrong happened.
+                pass
+
+            char = description_match.group(1)
+            description = description_match.group(2)
+            if description is None:
+                description = char
+            else:
+                description = char + u' ' + description
+
+            # Adding the item.
+            items.append((char, description))
+
+            # Removing item from line.
+            line = line[description_match.end():].strip()
+
+        if title is None:
+            # Auto-generating a title by concatenating all chars.
+            title = u''.join(item[0] for item in items)
+
+        return (title, items)
+
+    def parse_config_file(self):
         try:
-            charDef = open(self.CHARS_PATH).readlines()
+            with open(self.CHARS_PATH) as f:
+                # Reading and ignoring whitespace.
+                charDef = [ line.strip() for line in f.readlines() ]
+                # Ignoring blank lines and comments.
+                charDef = [ line for line in charDef if line and line[0] != '#' ]
         except IOError:
             charDef = []
 
+        for charLine in charDef:
+            yield self.parse_line(charLine)
+
+    def update_menu(self, widget=None, data=None):
         # Create menu
         menu = gtk.Menu()
         self.ind.set_menu(menu)
-        
-        for charLine in charDef:
-            charLine = unicode(charLine)
-            charLine = charLine.strip()
-            submenu_match = self.submenu_title_pattern.match(charLine)
-            if submenu_match:
-                submenu_title = submenu_match.group(1)
-                # remove title part from remainder:
-                charLine = charLine[submenu_match.end():]
-            else:
-                submenu_title = ''.join(
-                    self.description_pattern.split(charLine)[::2])
-            parentItem = self.create_menu_item(submenu_title)
+
+        for title, items in self.parse_config_file():
+            parentItem = self.create_menu_item(title)
             subMenu = gtk.Menu()
-            while charLine:
-                char = charLine[0]
-                charLine = charLine[1:]
-                description_match = self.description_pattern.match(charLine)
-                if description_match:
-                    item_title = char + ' ' + description_match.group(1)
-                    # remove description part from remainder:
-                    charLine = charLine[description_match.end():]
-                else:
-                    item_title = char
-                subItem = self.create_menu_item(item_title)
-                subItem.connect("activate", self.on_char_click, char)
+
+            for char, desc in items:
+                subItem = self.create_menu_item(desc)
+                subItem.connect('activate', self.on_char_click, char)
                 subMenu.append(subItem)
+
             parentItem.set_submenu(subMenu)
             menu.append(parentItem)
 
         menu.append(gtk.SeparatorMenuItem())
         quit_item = self.create_menu_item('Quit')
-        quit_item.connect("activate", self.on_quit)
+        quit_item.connect('activate', self.on_quit)
         menu.append(quit_item)
 
         # Show the menu
         menu.show_all()
 
     def on_char_click(self, widget, char):
-        cb = gtk.Clipboard(selection="PRIMARY")
+        cb = gtk.Clipboard(selection='PRIMARY')
         cb.set_text(char)
-        cb = gtk.Clipboard(selection="CLIPBOARD")
+        cb = gtk.Clipboard(selection='CLIPBOARD')
         cb.set_text(char)
 
     def on_quit(self, widget):
         gtk.main_quit()
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
+    if len(sys.argv) >= 2 and sys.argv[1] == '--test':
+        import doctest
+        failure_count, test_count = doctest.testmod()
+        print '{0} of {1} tests have failed'.format(failure_count, test_count)
+        if failure_count > 0:
+            sys.exit(1)
+        else:
+            sys.exit(0)
+
     # Catch CTRL-C
     signal.signal(signal.SIGINT, lambda signal, frame: gtk.main_quit())
 
     # Run the indicator
     i = IndicatorChars()
-    
-    # Monitor bookmarks changes 
+
+    # Monitor bookmarks changes
     file = gio.File(i.CHARS_PATH)
     monitor = file.monitor_file()
-    monitor.connect("changed", i.on_chars_changed)            
-    
+    monitor.connect('changed', i.on_chars_changed)
+
     # Main gtk loop
     gtk.main()
